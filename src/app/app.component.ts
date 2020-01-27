71768a4c9206359b4068251bfb551543c8296a84
Renato Sanches <cwebmundi@gmail.com>
2020-01-27 15:52:51 -0200

    removendo lista do app padrao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+// app.components é o controlador
+// Faz o controle do menu
+
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -16,13 +19,13 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
+  //Construtor da classe
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,34 @@
+//Todas as classes utilizadas no modulo devem ser importadas no import
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
+  //São as paginas, classes e componentes de um modulo
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage    
   ],
   imports: [
+    //lista de modulos que sao importados pelo modulo (NgModule)
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    //que segue a mesma linha do declarations
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
+    //declara as classes no qual os objetos injetados utilizem a mesma instancia
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
