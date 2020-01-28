dff154db3c624fe6399c6a708e3a93ce4b037474
Renato Sanches <cwebmundi@gmail.com>
2020-01-28 08:42:45 -0200

    desativa menu na tela de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,7 @@
 
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 // Controlador da view que é o home.html
 // o Component faz com que o 
 
@@ -17,11 +18,21 @@ import { NavController, IonicPage } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+// No construtor é que sao injetadas as dependencias como NavController e MenuController
+  constructor(public navCtrl: NavController, public menu: MenuController) {
   }
 
+  //Desabilita o menu quando entrar na pagina / evita arrastar do menu
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  //Habilita o menu quando sair na pagina
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  //No login acessa CategoriasPage
   login() {
     this.navCtrl.setRoot('CategoriasPage');
   }
