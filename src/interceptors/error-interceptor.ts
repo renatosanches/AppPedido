c085235acab9553362d4b76f0b3c71cffffaefee
Renato Sanches <cwebmundi@gmail.com>
2020-01-29 16:24:58 -0200

    Tratamento de erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 /* 
 * App Pedido - XP IT Tecnologia - Front End
@@ -13,10 +14,10 @@ import { StorageService } from '../services/storage.service';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {        
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
         .catch((error, caught) => {
 
@@ -30,12 +31,21 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
-
-            // Tratamento do Erro 403
+            
             switch(errorObj.status) {
+                // Tratamento do Erro 401
+                case 401:
+                this.handle401();
+                break;
+
+                // Tratamento do Erro 403
                 case 403:
                 this.handle403();
                 break;
+
+                //erros gerais
+                default:
+                this.handleDefaultEror(errorObj);
             }
 
             return Observable.throw(errorObj);
@@ -45,6 +55,36 @@ export class ErrorInterceptor implements HttpInterceptor {
     // funcao auxiliar (metodo da classe ErrorInterceptor) - forca a limpeza do local storage
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    // funcao auxiliar erro 401 - autenticação
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    // Tratamento de outros erros
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();        
     }
 }
 
