9e745c8ab61f7cccaf0433b33cf24ad4ff8eaf12
Renato Sanches <cwebmundi@gmail.com>
2020-01-31 10:06:57 -0200

    Tela Condicao de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -31,7 +31,7 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
-          
+
           let cart = this.cartService.getCart();
 
           this.pedido = {
@@ -55,7 +55,8 @@ export class PickAddressPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id: item.id};
-    console.log(this.pedido); 
+    // Faz navecagao passando dados do pedido 
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido}); 
   }
 
 }
